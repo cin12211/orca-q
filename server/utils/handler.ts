de68ff9d427ea941a9ef910c148ef59c093602eb
chinh.nguyen <chinh.nguyen@team.enouvo.com>
2025-04-30 20:17:32 +0700

    feat: update server api

---
+++ server/utils/handler.ts
@@ -0,0 +1,20 @@
+import type { EventHandler, EventHandlerRequest } from 'h3';
+
+//TODO: need to check
+export const defineWrappedResponseHandler = <T extends EventHandlerRequest, D>(
+  handler: EventHandler<T, D>
+): EventHandler<T, D> =>
+  defineEventHandler<T>(async event => {
+    try {
+      // do something before the route handler
+      const response = await handler(event);
+
+      console.log('response::::', response);
+
+      // do something after the route handler
+      return { response };
+    } catch (err) {
+      // Error handling
+      return { err };
+    }
+  });
