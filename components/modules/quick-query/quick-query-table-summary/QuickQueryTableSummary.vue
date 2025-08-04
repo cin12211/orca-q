5f5859aefcafbcf7c91281517eea65c0aaea782e
Thien Pham <phamanhthien1907@gmail.com>
2025-08-04 13:13:31 +0700

    feat: show table summary when no row selected

---
+++ components/modules/quick-query/quick-query-table-summary/QuickQueryTableSummary.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { Field, useForm } from 'vee-validate';
+
+type Props = {
+  summary: {
+    tableSize?: string;
+    dataSize?: string;
+    indexSize?: string;
+  };
+};
+
+const { summary } = defineProps<Props>();
+</script>
+
+<template>
+  <div class="flex flex-col gap-2 text-xs p-2">
+    <div class="flex items-center gap-2">
+      <span>Table Size:</span>
+      <span>{{ summary?.tableSize }}</span>
+    </div>
+    <div class="flex items-center gap-2">
+      <span>Data Size:</span>
+      <span>{{ summary?.dataSize }}</span>
+    </div>
+    <div class="flex items-center gap-2">
+      <span>Index Size:</span>
+      <span>{{ summary?.indexSize }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped></style>
