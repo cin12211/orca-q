984c9ab25c3e89e3c9dd8ceb61846ee0d15a378b
chinh.nguyen <chinh.nguyen@team.enouvo.com>
2026-04-07 23:51:37 +0700

    feat(): fix build in mac

--- layouts/home.vue
+++ layouts/home.vue
@@ -36,7 +36,7 @@ const onTitleBarDoubleClick = async () => {
 
         <p class="text-xl font-medium">orcaq</p>
       </div>
-      {{ 'show 1.0.47 → 1.0.48' }}
+      {{ 'show 1.0.48 → 1.0.49' }}
     </div>
   </div>
 
