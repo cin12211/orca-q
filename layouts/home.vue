ede1eb5620b68a1b501cd6390d87c8399f5e540d
chinh.nguyen <chinh.nguyen@team.enouvo.com>
2026-04-13 07:59:53 +0700

    feat(): enhance updater for elec app

--- layouts/home.vue
+++ layouts/home.vue
@@ -36,7 +36,7 @@ const onTitleBarDoubleClick = async () => {
 
         <p class="text-xl font-medium">orcaq</p>
       </div>
-      {{ 'show 1.0.57 → 1.0.58' }}
+      {{ 'show 1.0.58 → 1.0.59' }}
     </div>
   </div>
 
