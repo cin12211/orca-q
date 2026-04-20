2a3d56b33e0e954783c80084d9cc414c1bc9e222
chinh.nguyen <chinh.nguyen@team.enouvo.com>
2026-04-20 09:19:45 +0700

    feat(): init update v .62

--- layouts/home.vue
+++ layouts/home.vue
@@ -40,7 +40,7 @@ const githubLink = config.public.githubLink;
 
         <p class="text-xl font-medium">orcaq</p>
       </div>
-      {{ 'show 1.0.60 → 1.0.61' }}
+      {{ 'show 1.0.61 → 1.0.62' }}
     </div>
   </div>
 
